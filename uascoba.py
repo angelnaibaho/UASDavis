d6a3b62bd553f4d2b2ff794b3c989ba0300f636d
Angel Naibaho <101439456+angelnaibaho@users.noreply.github.com>
2024-06-20 22:49:29 +0700

    Update uascoba.py

--- uascoba.py
+++ uascoba.py
@@ -74,7 +74,7 @@ with st.sidebar:
 # Grafik
 if selected == 'Grafik':
     # Year selection above the "GRAFIK" heading
-    year = st.selectbox("Select Year", options=[2011, 2012, 2013, 2014], index=0)
+    year = st.selectbox("Select Year", options=[2001, 2002, 2003, 2004], index=0)
     
     st.write("""# GRAFIK""")
     query = """
