84e10b0245cdace0ad7aa8b7f0920dcc9d0ed0c5
Angel Naibaho <101439456+angelnaibaho@users.noreply.github.com>
2024-06-21 02:39:50 +0700

    Update uascoba.py

--- uascoba.py
+++ uascoba.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
@@ -66,101 +67,57 @@ def plot_bubble_chart(data):
 
     return fig
 
-def plot_stacked_column_chart(data):
-    pivot_data = data.pivot(index='SalesTerritoryRegion', columns='Gender', values='CustomerCount')
+# nav sidebar
+with st.sidebar:
+    selected = option_menu("Angel Dashboard", ['Grafik', 'Book Scrap'],
+        icons=['film', 'book', 'chart'], menu_icon="house", default_index=0)
 
-    plt.style.use('dark_background')
-    fig, ax = plt.subplots(figsize=(10, 6))
-    pivot_data.plot(kind='bar', stacked=True, ax=ax)
-
-    ax.set_title("Customer Count by Sales Territory Region and Gender (Stacked Column Chart)", color='white')
-    ax.set_xlabel('Sales Territory Region', color='white')
-    ax.set_ylabel('Customer Count', color='white')
-
-    ax.legend(title='Gender', facecolor='black', edgecolor='white', loc='upper right')
-    ax.set_facecolor('black')
-    fig.patch.set_facecolor('black')
-    fig.patch.set_edgecolor('white')
-
-    ax.tick_params(colors='white', which='both')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-
-    return fig
-
-# Function to fetch and display data
-def main():
-    st.sidebar.title('Angel Dashboard')
-    selected = st.sidebar.selectbox("Select Dashboard", ['Grafik', 'Book Scrap'], index=0)
-
-    if selected == 'Grafik':
-        st.write("""# GRAFIK""")
+# Grafik
+if selected == 'Grafik':
+    # Year selection above the "GRAFIK" heading
+    year = st.selectbox("Select Year", options=[2001, 2002, 2003, 2004], index=0)
+    
+    st.write("""# GRAFIK""")
+    query = """
+    SELECT 
+        dpc.EnglishProductCategoryName AS `Product Category`, 
+        gen.Gender AS Gender,
+        COUNT(fs.OrderQuantity) AS Quantity 
+    FROM 
+        factinternetsales fs 
+    JOIN dimproduct dp ON dp.ProductKey = fs.ProductKey 
+    JOIN dimproductsubcategory dsc ON dp.ProductSubcategoryKey = dsc.ProductSubcategoryKey 
+    JOIN dimproductcategory dpc ON dsc.ProductCategoryKey = dpc.ProductCategoryKey 
+    JOIN dimcustomer gen ON fs.CustomerKey = gen.CustomerKey
+    GROUP BY 
+        dpc.EnglishProductCategoryName,
+        gen.Gender
+    ORDER BY 
+        Quantity;
+    """
+    data = fetch_data_from_db(query)
+    if data is not None:
+        col1, col2 = st.columns(2)
         
-        query_histogram = """
-        SELECT 
-            dpc.EnglishProductCategoryName AS `Product Category`, 
-            gen.Gender AS Gender,
-            COUNT(fs.OrderQuantity) AS Quantity 
-        FROM 
-            factinternetsales fs 
-        JOIN dimproduct dp ON dp.ProductKey = fs.ProductKey 
-        JOIN dimproductsubcategory dsc ON dp.ProductSubcategoryKey = dsc.ProductSubcategoryKey 
-        JOIN dimproductcategory dpc ON dsc.ProductCategoryKey = dpc.ProductCategoryKey 
-        JOIN dimcustomer gen ON fs.CustomerKey = gen.CustomerKey
-        GROUP BY 
-            dpc.EnglishProductCategoryName,
-            gen.Gender
-        ORDER BY 
-            Quantity;
-        """
-
-        query_bubble = """
-        SELECT 
-            dimsalesterritory.SalesTerritoryRegion,  
-            COUNT(dimcustomer.CustomerKey) AS CustomerCount 
-        FROM  
-            dimgeography 
-        JOIN dimcustomer ON dimgeography.GeographyKey = dimcustomer.GeographyKey 
-        JOIN dimsalesterritory ON dimgeography.SalesTerritoryKey = dimsalesterritory.SalesTerritoryKey
-        GROUP BY   
-            dimgeography.SalesTerritoryKey, dimsalesterritory.SalesTerritoryRegion
-        ORDER BY  
-            CustomerCount;
-        """
-
-        query_stacked_column = """
-        SELECT 
-            dimsalesterritory.SalesTerritoryRegion,  
-            gen.Gender,
-            COUNT(dimcustomer.CustomerKey) AS CustomerCount 
-        FROM  
-            dimgeography 
-        JOIN dimcustomer ON dimgeography.GeographyKey = dimcustomer.GeographyKey 
-        JOIN dimsalesterritory ON dimgeography.SalesTerritoryKey = dimsalesterritory.SalesTerritoryKey
-        GROUP BY   
-            dimgeography.SalesTerritoryKey, dimsalesterritory.SalesTerritoryRegion, gen.Gender
-        ORDER BY  
-            dimsalesterritory.SalesTerritoryRegion;
-        """
-
-        data_histogram = fetch_data_from_db(query_histogram)
-        data_bubble = fetch_data_from_db(query_bubble)
-        data_stacked_column = fetch_data_from_db(query_stacked_column)
-
-        if data_histogram is not None:
-            st.subheader("Histogram: Total Quantity by Product Category and Gender")
-            fig1 = plot_histogram(data_histogram)
+        with col1:
+            fig1 = plot_histogram(data)
             st.pyplot(fig1)
 
-        if data_bubble is not None:
-            st.subheader("Bubble Chart: Customer Count by Sales Territory Region")
-            fig2 = plot_bubble_chart(data_bubble)
-            st.pyplot(fig2)
-
-        if data_stacked_column is not None:
-            st.subheader("Stacked Column Chart: Customer Count by Sales Territory Region and Gender")
-            fig3 = plot_stacked_column_chart(data_stacked_column)
-            st.pyplot(fig3)
-
-if __name__ == "__main__":
-    main()
+        with col2:
+            query_bubble = """
+            SELECT 
+                dimsalesterritory.SalesTerritoryRegion,  
+                COUNT(dimcustomer.CustomerKey) AS CustomerCount 
+            FROM  
+                dimgeography 
+            JOIN dimcustomer ON dimgeography.GeographyKey = dimcustomer.GeographyKey 
+            JOIN dimsalesterritory ON dimgeography.SalesTerritoryKey = dimsalesterritory.SalesTerritoryKey
+            GROUP BY   
+                dimgeography.SalesTerritoryKey, dimsalesterritory.SalesTerritoryRegion
+            ORDER BY  
+                CustomerCount;
+            """
+            data_bubble = fetch_data_from_db(query_bubble)
+            if data_bubble is not None:
+                fig2 = plot_bubble_chart(data_bubble)
+                st.pyplot(fig2)
